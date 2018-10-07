39f64026f33e864c4808424f5ce001eefb6628e7
Jay Fox <abelianfox@gmail.com>
2018-10-07 19:11:54 +0200

    Problem 142: second attempt
    But algorithm is still too slow - needs refinement.

--- C# Code/EulerProject/Problems/Problem142.cs
+++ C# Code/EulerProject/Problems/Problem142.cs
@@ -10,20 +10,51 @@ namespace ProjectEuler
         public static long limit = 100;
         public static long Solution()
         {
-            long solution = 0;
             for (long sum = 6; ; sum++)
             {
-                for (long z = 1; z < sum / 3.0; z++)
-                    for (long y = z + 1; y < (sum - z) / 2.0; y++)
+                var bLimit = Math.Sqrt(2 * sum - 7);
+               for (long b = (long)Math.Sqrt(2*sum/3)+1; b <= bLimit; b++)
+                {
+                    var dLimit = Math.Sqrt(2 * sum - b - 1);
+                    for (long d = (long)Math.Sqrt((2*sum - b)/2)+1; d <= dLimit; d++)
                     {
-                        long x = sum - y - z;
-                        if (UtilityFunctions.IsPerfectSquare(x - y) && UtilityFunctions.IsPerfectSquare(x + y) && UtilityFunctions.IsPerfectSquare(y - z) && UtilityFunctions.IsPerfectSquare(y + z) && UtilityFunctions.IsPerfectSquare(x - z) && UtilityFunctions.IsPerfectSquare(x + z))
+                        if (!UtilityFunctions.IsPerfectSquare(2 * sum - b - d))
+                            break;
+                        long fSquared= 2 * sum - b - d;
+
+                        if (fSquared <= 4 || fSquared >= d * d || d >= b) 
+                            break;
+                        for (long c = 2 - (d % 2); c < d; c += 2) 
                         {
-                            Console.WriteLine($"{x} {y} {z}");
-                            return sum;
+                            var eLimit = Math.Min(c, Math.Sqrt(fSquared));
+                            for (long e = 12 + (fSquared % 2); e < eLimit; e++)
+                            {
+                                var cSquared = c * c;
+                                var dSquared = d * d;
+                                var eSsuared = e * e;
+                                if (dSquared - cSquared != fSquared - eSsuared)
+                                    break;
+                                for (long a = 2 - (b % 2); a < c; a += 2) 
+                                {
+                                    var aSquared = a * a;
+                                    var bSquared = b * b;
+                                    
+                                    var x = aSquared + bSquared;
+                                    var y = bSquared - aSquared;
+                                    var z = dSquared - cSquared;
+                                    //Console.WriteLine($"{sum} : {b} {d} {f} {e} | {c} {a} | {e}");
+                                    if ((x == cSquared + dSquared) && (y == eSsuared + fSquared) && (z == fSquared - eSsuared))
+                                    {
+                                        Console.WriteLine($"Solution: {x / 2} {y / 2} {z / 2} {sum / 2}");
+                                        return sum / 2;
+                                    }
+                                }
+                            }
                         }
                     }
-                Console.WriteLine(sum);
+                }
+                if (sum % 1000 == 0)
+                    Console.WriteLine(sum);
             }
         }
     }
