b44a0b3bb9b9125c749cd5f1de8148f72ff30bf3
Jay Fox <abelianfox@gmail.com>
2018-10-24 23:12:32 +0200

    Eliminated costly Max operations to speed up the solution from 5 seconds to 200ms.

--- C# Code/EulerProject/Problems/Problem500.cs
+++ C# Code/EulerProject/Problems/Problem500.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 
 namespace ProjectEuler
@@ -10,30 +11,40 @@ namespace ProjectEuler
         public static long limit = 500500;
         public static long Solution()
         {
+            var stopwatch = new Stopwatch();
             long solution = 1;
             var exponents = new List<int>();
             var primes = UtilityFunctions.Primes(7376512);
-            var candidates = new List<long>(primes);
+            var candidates = new List<long> { 1 };
 
             for (int i = 0; i < primes.Count; i++)
             {
                 var primePower = primes[i]*primes[i];
-                var max = candidates.Max();
-                if (primePower > max)
+                var maxCandidate = candidates.Max();
+
+                if (primePower > primes.Last() && primePower > maxCandidate)
                     break;
-                while (primePower < max)
+                while (primePower < Math.Max(primes.Last(), maxCandidate))
                 {
                     candidates.Add(primePower);
-                    candidates.Remove(max);
-                    max = candidates.Max();
+                    if (maxCandidate > primes.Last())
+                    {
+                        candidates.Remove(maxCandidate);
+                        maxCandidate = candidates.Max();
+                    }
+                    else
+                    {
+                        primes.RemoveAt(primes.Count - 1);
+                    }
                     primePower *= primePower;
                 }
                 
             }
-
-            foreach (var t in candidates)
+            
+            foreach (var t in candidates.Concat(primes))
                 solution = (solution * t) % 500500507;
-
+            
+            Console.WriteLine($"Time spent finding max {stopwatch.ElapsedMilliseconds}");
             return solution;
         }
     }
