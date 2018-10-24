3d07d99b6e5ffd4180293b4b66f5824102dfed3b
Jay Fox <abelianfox@gmail.com>
2018-10-24 18:56:01 +0200

    Problem 347 solved!

---
+++ C# Code/EulerProject/Problems/Problem347.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ProjectEuler
+{
+    public class Problem347
+    {
+        public static long limit = UtilityFunctions.IntegralPower(10,7);
+        public static long Solution()
+        {
+            long solution = 0;
+            var primes = UtilityFunctions.Primes(limit/2);
+
+            for (int i = 0; i < primes.Count; i++)
+            {
+                var p = primes[i];
+                List<long> powersOfP = new List<long>();
+                var n = p;
+                while (n < limit/p)
+                {
+                    powersOfP.Add(n);
+                    n *= p;
+                }
+                for (int j = i+1; j < primes.Count; j++)
+                {
+                    var q = primes[j];
+                    if (p * q > limit)
+                    {
+                        break;
+                    }
+                    var powersOfPTimesQ = new List<long>(powersOfP);
+                    long max = 0;
+                    while (true)
+                    {
+                        powersOfPTimesQ = powersOfPTimesQ.Where(t => t * q <= limit).Select(t => t * q).ToList();
+                        if (powersOfPTimesQ.Count == 0)
+                            break;
+                        max = Math.Max(powersOfPTimesQ.Max(), max);
+                    }
+                    solution += max;
+                }
+            }
+
+
+
+            return solution;
+        }
+    }
+}
