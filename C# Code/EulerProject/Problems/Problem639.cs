c0d11e82cc9c32abf406dd127a27e75e2e429580
Jay Fox <abelianfox@gmail.com>
2018-10-29 02:08:14 +0200

    The memory management, it defeats me.

--- C# Code/EulerProject/Problems/Problem639.cs
+++ C# Code/EulerProject/Problems/Problem639.cs
@@ -10,7 +10,7 @@ namespace ProjectEuler
 {
     public class Problem639
     {
-        public static long limit = UtilityFunctions.IntegralPower(10,4);
+        public static long limit = UtilityFunctions.IntegralPower(10,8);
         public static int exponent = 3;
         public static long modBase = 1000000007;
 
@@ -19,97 +19,30 @@ namespace ProjectEuler
             var stopwatch = new Stopwatch();
             long solution = 0;
             var primes = UtilityFunctions.Primes((int)Math.Sqrt(limit));
-            var nonSquareFree = new Dictionary<long,long>();
-          
-            stopwatch.Restart();
-            Parallel.ForEach(primes, (p) =>
-            {
-                var primePower = p * p;
-                
-                for (long i = 1; i <= limit / primePower; i++)
-                {
-                    lock (nonSquareFree)
-                    {
-                        var newNumber = i * primePower;
-                        if (nonSquareFree.ContainsKey(newNumber))
-                        {
-                            nonSquareFree[newNumber] *= p;
-                        }
-                        else
-                            nonSquareFree.Add(newNumber,p);
-                    }
-                }
-            });
-            stopwatch.Stop();
-            Console.WriteLine($"Square-free part list took {stopwatch.ElapsedMilliseconds}.");
-            primes = null;
-            GC.Collect();
-            var squareFreePart = new int[modBase/2];
-
-            stopwatch.Restart();
+            var squareFreePart = new long[limit];
+           
+           
+            stopwatch.Start();
             for (long i = 1; i <= limit; i++)
             {
-                
-                var reduced = nonSquareFree.ContainsKey(i)? nonSquareFree[i] % modBase : i% modBase;
-                    squareFreePart[reduced]++;
+                squareFreePart[i - 1] = i;
             }
             stopwatch.Stop();
-            Console.WriteLine($"Reducing square-free list took {stopwatch.ElapsedMilliseconds}.");
-
+            Console.WriteLine($"Initialising list took {stopwatch.ElapsedMilliseconds}.");
             stopwatch.Restart();
-            for (long i = 0; i < Math.Min(modBase, limit); i++)
+           
+            foreach (var p in primes)
             {
-
-                var count = squareFreePart[i];
-                var number = i+1;
-                if (count == 0)
-                    continue;
-
-                long sum;
-                UtilityFunctions.Gcd(number - 1, modBase, out long inverse, out long dummy);
-                if (inverse == 0)
-                    sum = exponent;
-                else
+                var primePower = p * p;
+                while (primePower <= limit)
                 {
-                    var modPower = UtilityFunctions.ModPower(number, exponent, modBase);
-                    sum = ((((modPower - 1) * ((inverse + modBase) % modBase)) % modBase) * number) % modBase;
+                    for (long i = 1; i <= limit / primePower; i++)
+                    {
+                        squareFreePart[i * primePower - 1] /= p;
+                    }
+                    primePower *= p;
                 }
-                solution = (solution + (sum * count) % modBase) % modBase;
             }
-            stopwatch.Stop();
-            Console.WriteLine($"Processing took {stopwatch.ElapsedMilliseconds}.");
-            return solution;
-        }
-
-        public static long Solution2()
-        {
-            var stopwatch = new Stopwatch();
-            long solution = 0;
-            var primes = UtilityFunctions.Primes((int)Math.Sqrt(limit));
-            var squareFreePart = new long[limit];
-           stopwatch.Start();
-           for (long i = 1; i<=limit; i++)
-           {
-               squareFreePart[i - 1] = i;
-           }
-           stopwatch.Stop();
-           Console.WriteLine($"Initialising list took {stopwatch.ElapsedMilliseconds}.");
-            stopwatch.Restart();
-            Parallel.ForEach(primes, (p) =>
-           {
-               var primePower = p * p;
-               while (primePower <= limit)
-               {
-                   for (long i = 1; i <= limit / primePower; i++)
-                   {
-                      lock(squareFreePart) 
-                      {
-                          squareFreePart[i * primePower - 1] /= p;
-                      }
-                   } 
-                   primePower *= p;
-               }
-           });
             stopwatch.Stop();
             Console.WriteLine($"Square-free part list took {stopwatch.ElapsedMilliseconds}.");
             stopwatch.Restart();
@@ -129,26 +62,122 @@ namespace ProjectEuler
             Console.WriteLine($"Reducing square-free list took {stopwatch.ElapsedMilliseconds}.");
 
             stopwatch.Restart();
-            for (long i = 0; i < Math.Min(modBase,limit); i++)
-           {
-               var count = squareFreePart[i];
+            for (long i = 0; i < Math.Min(modBase, limit); i++)
+            {
+                var count = squareFreePart[i];
                 if (count == 0)
                     continue;
 
-               long inverse, sum;
-               UtilityFunctions.Gcd(i, modBase, out inverse, out long dummy);
-               if (inverse == 0)
-                   sum = exponent;
-               else
-               {
-                   var modPower = UtilityFunctions.ModPower(i+1, exponent, modBase);
-                   sum = ((((modPower - 1) * ((inverse + modBase) % modBase)) % modBase) * (i+1)) % modBase;
-               }
-               solution = (solution + (sum*count) % modBase) % modBase;
-           }
+                long inverse, sum;
+                UtilityFunctions.Gcd(i, modBase, out inverse, out long dummy);
+                if (inverse == 0)
+                    sum = exponent;
+                else
+                {
+                    var modPower = UtilityFunctions.ModPower(i + 1, exponent, modBase);
+                    sum = ((((modPower - 1) * ((inverse + modBase) % modBase)) % modBase) * (i + 1)) % modBase;
+                }
+                solution = (solution + (sum * count) % modBase) % modBase;
+            }
             stopwatch.Stop();
             Console.WriteLine($"Processing took {stopwatch.ElapsedMilliseconds}.");
+
+            // var count = limit / modBase;
+            // var residue = limit % modBase;
+            // for (long i = 1; i < Math.Min(modBase, limit); i++)
+            // {
+            //     var thisCount = i < residue ? count + 1 : count;
+            //
+            //     long inverse, sum;
+            //     UtilityFunctions.Gcd(i, modBase, out inverse, out long dummy);
+            //     if (inverse == 0)
+            //         sum = exponent;
+            //     else
+            //     {
+            //         var modPower = UtilityFunctions.ModPower(i + 1, exponent, modBase);
+            //         sum = ((((modPower - 1) * ((inverse + modBase) % modBase)) % modBase) * (i + 1)) % modBase;
+            //     }
+            //     solution = (solution + (sum * thisCount) % modBase) % modBase;
+            // }
+
             return solution;
         }
+
+
+        public static long Solution2()
+        {
+            var chunks = Math.Max(limit/100000000,1);
+            
+
+            var stopwatch = new Stopwatch();
+            long solution = 0;
+            var primes = UtilityFunctions.Primes((int)Math.Sqrt(limit));
+            
+
+            for (int batch = 0; batch < chunks; batch++)
+            {
+                Console.WriteLine($"Processing batch {batch + 1}.");
+                var lowerLimit = batch * (limit / chunks);
+                var upperLimit = lowerLimit + limit / chunks;
+                var nonSquareFree = new Dictionary<long, long>();
+                stopwatch.Restart();
+                //Parallel.ForEach(primes, (p) =>
+                foreach (var p in primes)
+                {
+
+                    var primePower = p * p;
+                    while (primePower <= upperLimit)
+                    {
+                        for (long i = lowerLimit / primePower + 1; i <= upperLimit / primePower; i++)
+                        {
+                            var newNumber = i * primePower;
+                            if (nonSquareFree.ContainsKey(newNumber))
+                            {
+                                nonSquareFree[newNumber] /= p;
+                            }
+                            else
+                                nonSquareFree.Add(newNumber, newNumber/p);
+                        }
+                        primePower *= p;
+                    }
+                }
+                stopwatch.Stop();
+                Console.WriteLine($"Square-free part list took {stopwatch.ElapsedMilliseconds}.");
+
+               // stopwatch.Restart();
+               // for (long i = 1; i <= limit; i++)
+               // {
+               //
+               //     var reduced = nonSquareFree.ContainsKey(i) ? nonSquareFree[i] % modBase : i % modBase;
+               //     squareFreePart[reduced]++;
+               // }
+               // stopwatch.Stop();
+               // Console.WriteLine($"Reducing square-free list took {stopwatch.ElapsedMilliseconds}.");
+
+                stopwatch.Restart();
+               // for (long i = lowerLimit+1; i <= upperLimit; i++)
+               // {
+               //
+               //     var number = nonSquareFree.ContainsKey(i)? nonSquareFree[i] : i;
+               //
+               //     long sum;
+               //     UtilityFunctions.Gcd(number - 1, modBase, out long inverse, out long dummy);
+               //     if (inverse == 0)
+               //         sum = exponent;
+               //     else
+               //     {
+               //         var modPower = UtilityFunctions.ModPower(number, exponent, modBase);
+               //         sum = ((((modPower - 1) * ((inverse + modBase) % modBase)) % modBase) * number) % modBase;
+               //     }
+               //     solution = (solution + (sum) % modBase) % modBase;
+               // }
+               
+                stopwatch.Stop();
+                Console.WriteLine($"Processing took {stopwatch.ElapsedMilliseconds}.");
+            }
+            return solution;
+        }
+
+        
     }
 }
